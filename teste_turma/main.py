fdc74c5357b3ebf0127d5c66b684945d3cb1fe12
Fernanda Barbalho <137642560+fernandalopesbarbalho@users.noreply.github.com>
2025-10-12 11:04:20 -0300

    Add files via upload

---
+++ teste_turma/main.py
@@ -0,0 +1,17 @@
+# Atividade: A partir do código do professor, corrigir o método cadastrarAlunos e criar o testIntervalo
+
+import aluno as a;
+import turma as t;
+
+alunos = []
+alunos.append(a.Aluno('Fabio', 'Teixeira', 8))
+alunos.append(a.Aluno('Fabiano', 'Teixeira', 10))
+alunos.append(a.Aluno('Melissa', 'Teixeira', -1))
+
+turmaObject = t.Turma()
+turmaObject.cadastrarAlunos(alunos)
+
+turmaObject.mostrarAlunos()
+print('*'*30)
+print('Aluno com menor nota:', turmaObject.menorNota.mostrarAluno())
+print('Aluno com maior nota:', turmaObject.maiorNota.mostrarAluno())
