42595d5c6251e0224cee2daa86f5d54db648617b
Fernanda Barbalho <137642560+fernandalopesbarbalho@users.noreply.github.com>
2025-11-17 20:33:41 -0300

    Update main.py

--- teste_integracao/main.py
+++ teste_integracao/main.py
@@ -2,7 +2,7 @@ import aluno as a;
 import turma as t;
 
 alunos = []
-alunos.append(a.Aluno('Fabio', 'Teixeira', 11));
+alunos.append(a.Aluno('Fabio', 'Teixeira', 10));
 alunos.append(a.Aluno('Fabiano', 'Teixeira', 7));
 alunos.append(a.Aluno('Melissa', 'Teixeira', 8));
 alunos.append(a.Aluno('Rafael', 'Teixeira', 9));
